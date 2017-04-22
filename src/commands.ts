0a6c6c51bdc57cef9a982cd783100c31220bd23d
Eric Amodio <eamodio@gmail.com>
2017-04-22 01:24:34 -0400

    Adds error logging

--- src/commands.ts
+++ src/commands.ts
@@ -1,4 +1,5 @@
 'use strict';
 export * from './commands/common';
 export * from './commands/keyboard';
+
 export * from './commands/showRelated';
--- src/quickPicks.ts
+++ src/quickPicks.ts
@@ -1,3 +1,4 @@
 'use strict';
 export * from './quickPicks/common';
+
 export * from './quickPicks/related';
